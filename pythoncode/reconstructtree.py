a398157b5ca3341b009c4280b42dfdba70cde5f0
luokui <379733643@qq.com>
2019-08-31 23:35:51 +0800

    add recon tree

---
+++ pythoncode/reconstructtree.py
@@ -0,0 +1,93 @@
+
+
+class node:
+    def __init__(self, value=None, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+
+class ReconstructTree:
+    def __init__(self):
+        pass
+
+    def fit(self, preorder, inorders):
+        # mynode = None
+        if len(preorder) != len(inorders):
+            raise ValueError("树的元素错误")
+
+        if len(preorder) == 1 and len(inorders) == 1:
+            if preorder[0] == inorders[0]:
+                return node(preorder[0])
+            else:
+                raise ValueError("输入不是用一颗树的不同遍历")
+
+        lens = len(preorder)
+
+        rootvalue = preorder[0]
+        mynode = node(rootvalue)
+        position = inorders.index(rootvalue)
+        right_lens = lens - position - 1
+
+        if position > 0:
+            temp_preorder = preorder[1: position + 1].copy()
+            temp_inorder = inorders[0: position].copy()
+            mynode.left = self.fit(temp_preorder, temp_inorder)
+        if right_lens > 0:
+            temp_preorder = preorder[position + 1:].copy()
+            temp_inorder = inorders[position+1:].copy()
+            mynode.right = self.fit(temp_preorder, temp_inorder)
+        return mynode
+
+    @classmethod
+    def print_value(cls, tree, test):
+        """
+        。。
+        :param tree: 一颗二叉树
+        :param test: 树节点值 按照特定的遍历方式 存入的列表
+        :return: 打印节点值
+        """
+        if tree.value is not None:
+            print(tree.value)
+            test.append(tree.value)
+        if tree.left is not None:
+            cls.print_value(tree.left, test)
+        if tree.right is not None:
+            cls.print_value(tree.right, test)
+
+    @classmethod
+    def compare(cls, lista, listb):
+        lens = len(lista)
+        if lens != len(listb):
+            raise ValueError("两个列表必须相等")
+        for i in range(lens):
+            if lista[i] != listb[i]:
+                print("not passed")
+                return
+        print("passed")
+        return
+
+
+if __name__ == "__main__":
+    preorderx = [1, 2, 4, 7, 3, 5, 6, 8]
+    # preorderx = [1, 2, 4, 7, 3, 5, 6, 8, 11]
+    # preorderx = [1, 2, 4, 7, 3, 5, 6]
+    # preorderx = [1, 2, 4, 7, 3, 5, 6, 11]
+    inorderx = [4, 7, 2, 1, 5, 3, 6, 8]
+
+    preorderx = [1, 2, 4, 7]
+    inorderx = [4, 7, 2, 1]
+
+    preorderx = [1, 3, 5, 6, 8]
+    inorderx = [1, 5, 3, 6, 8]
+
+    model = ReconstructTree()
+    res = model.fit(preorderx, inorderx)
+
+    tests = []
+    model.print_value(res, tests)
+    print("---", tests)
+
+    # model.compare(tests,[1, 5, 3, 6, 8])
+    model.compare(tests, preorderx)
+
