5314ebc1ba0264ce0799c6389a0f80b85850b374
luokui <379733643@qq.com>
2019-10-16 00:25:37 +0800

    update quicksort

--- pythoncode/insertsort.py
+++ pythoncode/insertsort.py
@@ -1,7 +1,12 @@
 
 
 class SortAlgorithm:
-    def insert_sort(self, data):
+    def __init__(self, data):
+        self.data = data
+        self.lens = len(data)
+
+    @staticmethod
+    def insert_sort(data):
         lens = len(data)
         if lens < 2:
             return data
@@ -14,7 +19,8 @@ class SortAlgorithm:
             data[index+1] = key
         return data
 
-    def bubble_sort(self, data):
+    @staticmethod
+    def bubble_sort(data):
         lens = len(data)
         if lens < 2:
             return data
@@ -27,11 +33,41 @@ class SortAlgorithm:
             index -= 1
         return data
 
+    def __quicksort(self, start, ends):
+        # xiao -> data
+        key = self.data[ends]
+        i = start - 1
+        for j in range(0, ends):
+            if self.data[j] <= key:
+                self.data[i+1], self.data[j] = self.data[j], self.data[i+1]
+                i += 1
+        self.data[i+1], self.data[ends] = self.data[ends], self.data[i+1]
+        return i+1
+
+    def quicksort(self, start=0, ends=5):
+        # lens = len(self.data)
+        #if start < ends:
+        position = self.__quicksort(start, ends)
+        if position - 1 > 0:
+            self.quicksort(0, position - 1)
+        if position + 1 < ends :
+            self.quicksort(position + 1, ends)
+        # self._quicksort(0, position)
+        # self._quicksort(position+1, self.lens-1)
+
+    @property
+    def sort_data(self):
+        return self.data
+
 
 if __name__ == "__main__":
-    data = [6, 2, 4, 3, 7, 1]
+    data = [6, 2, 4, 3, 7, 1, 8, 9, 4]
     # data = [5, 4, 2, 1]
 
-    print(SortAlgorithm().insert_sort(data))
+    print(SortAlgorithm.insert_sort(data))
 
-    print(SortAlgorithm().bubble_sort(data))
+    print(SortAlgorithm.bubble_sort(data))
+
+    my = SortAlgorithm(data)
+    my.quicksort(0, 5)
+    print(my.sort_data)
