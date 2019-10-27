5c24eaf3ca0a7e03c62dc749c5b2f591a75d288f
luokui <379733643@qq.com>
2019-10-27 23:01:16 +0800

    add

--- pythoncode/leetcode_420.py
+++ pythoncode/leetcode_420.py
@@ -55,13 +55,9 @@ class Solution:
             another += 1
         if not low:
             another += 1
-
-        print(record)
-
         print("another {}, counts {}, third_count {}".format(another, counts, third_count))
         if another == 0 and counts == 0 and third_count == 0:
             return 0
-
         if lens < 6:
             if third_count >= another:
                 temp = third_count - another
@@ -81,17 +77,22 @@ class Solution:
         # 需要删除的 third_counnt
         if lens > 20:
             if another >= counts:
+                print("---")
                 return third_count + another
             else:
                 temp = counts - another
-                # 至少删掉 5 - 3  4-2  6 - 4 7 -5  8-6
-                temp_ano = third_count % 3
 
-                print("temp_ano", temp_ano, temp)
-                if temp_ano > temp:
-                    return third_count + another
+                if third_count % 3 == 0:
+                    temp_anp = third_count // 3
                 else:
-                    return counts + third_count
+                    temp_anp = third_count // 3 + 1
+
+                if temp_anp > temp:
+                    print("dsd here")
+                    return temp_anp + another
+                else:
+                    print("there")
+                    return counts - temp_anp + third_count + 1
 
         if counts >= another:
             return counts
@@ -99,7 +100,8 @@ class Solution:
             return another
 
 if __name__ == "__main__":
-    data = "acd123A561111111111111"
+    data = "a11aaa12234111sdadas111"
+    # data = "111111111111111111111"
     print("length of data {}".format(len(data)))
     my = Solution()
     print(my.strongPasswordChecker(data))
--- pythoncode/leetcode_862.py
+++ pythoncode/leetcode_862.py
@@ -4,28 +4,62 @@ class Solution:
         lens = len(data)
         if lens < 1:
             return -1
-        if sum(data) < k:
+        x1 = 0
+        x2 = 0
+        sums = 0
+        starts = 0
+        flag = False
+
+        maxs_len = float("inf")
+
+        for ends in range(lens):
+            second = data[ends]
+            sums += second
+            if second < 0:
+                flag = True
+            if sums >= k:
+                if sums - data[starts] >= k:
+                    while sums - data[starts] >= k and starts < ends:
+                        sums -= data[starts]
+                        starts += 1
+                # 找到了一个子数组
+                print("from ", starts, " to", ends)
+                print("temp lens is {}".format(ends - starts + 1))
+                if maxs_len > ends - starts + 1:
+                    maxs_len = ends -starts + 1
+                    print(maxs_len, " maxlen")
+
+                if flag:
+                    temp_sum = sums
+                    temp_index = starts
+                    while temp_index <= ends:
+                        if temp_sum - data[temp_index] < k:
+                            temp_sum -= data[temp_index]
+                            temp_index += 1
+                        else:
+                            # 更新 sums 和 start
+                            temp_index += 1
+
+                            sums = temp_sum - data[temp_index]
+                            starts = temp_index
+
+                            temp_len = ends - temp_index + 1
+                            if temp_len < maxs_len:
+                                maxs_len = temp_len
+                    flag = False
+
+        print(x1, x2, "xxx")
+        if maxs_len == float("inf"):
             return -1
+        return maxs_len
 
-        if sum(data[:-1]) < k and sum(data[1:]) < k:
-            return len(data)
-
-        left = self.find(data[:-1], k)
-        right = self.find(data[1:], k)
-
-        if left <= right and left > -1:
-            return left
-        if left <= right and left == -1 and right != -1:
-            return right
-        if right < left and right > -1:
-            return right
-        if right < left and right == -1 and left != -1:
-            return left
 
 if __name__ == "__main__":
-    data = [1, 10, 2, -4, -3, 6, 2, 1]
-    # data = [2, -1, 2]
+    data = [3, -1, -1, 3, -1, 3]
+    # data = [4, 5, 6, 7, -1, 2, 4, 5, -4, 7]
+    # data = [2, -1, 2, 2]
+    # data = [2, -1, 2, 2]
+    k = 5
+    ##data = [2, -1, 2]
     my = Solution()
-    print(my.find(data, 9))
-
-
+    print("result ", my.find(data, k))
