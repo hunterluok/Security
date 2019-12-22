4efc0cc449780db73598fd0c365f2e971bf76e94
luokui <379733643@qq.com>
2019-12-22 22:49:18 +0800

    fix node

--- pythoncode/node.py
+++ pythoncode/node.py
@@ -15,9 +15,21 @@ class BinaryTreeNode:
 
 
 class ThreeNode:
-    def __init__(self, value=None, father=None, left=None, right=None):
+    def __init__(self, value=None, feature=None, father=None, left=None, right=None):
         self.value = value
+        self.feture = feature
         self.father = father
         self.left = left
         self.right = right
+
+
+class KdNode:
+    def __init__(self, data=None, value=None, feature=None, father=None, left=None, right=None):
+        self.data = data
+        self.value = value
+        self.feature = feature
+        self.father = father
+        self.left = left
+        self.right = right
+
 
