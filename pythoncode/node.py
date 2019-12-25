963a69faa7f7fa5dd1817fe5fc07abe7434a8d48
luokui <379733643@qq.com>
2019-12-25 23:12:53 +0800

    update node

--- pythoncode/node.py
+++ pythoncode/node.py
@@ -23,8 +23,24 @@ class ThreeNode:
         self.right = right
 
 
+# class KdNode:
+#     def __init__(self, data=None, value=None, feature=None, father=None, left=None, right=None):
+#         self.data = data
+#         self.value = value
+#         self.feature = feature
+#         self.father = father
+#         self.left = left
+#         self.right = right
+#
+
 class KdNode:
-    def __init__(self, data=None, value=None, feature=None, father=None, left=None, right=None):
+    def __init__(self,
+                 data=None,
+                 value=None,
+                 feature=None,
+                 father=None,
+                 left=None,
+                 right=None):
         self.data = data
         self.value = value
         self.feature = feature
